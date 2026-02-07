f398c89ac6973da620064ac63a55a9302b0d9310
studentmo <nikolaev@pc20513.ddut.local>
2026-02-07 18:59:04 +0300

    base structure
    Вы можете начать писать этот проект каждый по своему, используйти README как карту коммитов

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    //flags = argcProc(argc, *argv);
+    //numbers = fileCount(flags);
+    //ERRFLAG = resultOutput(numbers);
+    //return ERRFLAG;
+}
